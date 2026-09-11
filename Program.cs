a86d982c4e376747ad2bca9cfec23c858236fe24
ladel1 <adel.latibi@gmail.com>
2026-09-11 22:32:37 +0200

    Ajoute DES pour tester CodeQL

--- Program.cs
+++ Program.cs
@@ -9,12 +9,5 @@ string contenu = File.ReadAllText(fileName);
 
 Console.WriteLine(contenu);
 
-// Faille volontaire : MD5 est casse, on ne doit plus l'utiliser
-// pour du hachage de securite (mots de passe, signatures...).
-string motDePasse = "azerty123";
-byte[] octets = Encoding.UTF8.GetBytes(motDePasse);
-
-using MD5 md5 = MD5.Create();
-byte[] empreinte = md5.ComputeHash(octets);
-
-Console.WriteLine(Convert.ToHexString(empreinte));
+// Faille volontaire : DES est un algorithme de chiffrement obsolete.
+using DESCryptoServiceProvider des = new DESCryptoServiceProvider();
