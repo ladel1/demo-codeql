ab1a13aec5443f0fd48c2fe372903ba3e7255e19
ladel1 <adel.latibi@gmail.com>
2026-09-12 22:11:33 +0200

    Ajoute une dependance NuGet vulnerable

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
-
+using Newtonsoft.Json;
 
 string fileName = args[0];
 string contenu = File.ReadAllText(fileName);
@@ -11,3 +11,7 @@ Console.WriteLine(contenu);
 
 // Faille volontaire : DES est un algorithme de chiffrement obsolete.
 using DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+
+
+string json = JsonConvert.SerializeObject(new { Nom = "Alice" });
+Console.WriteLine(json);
